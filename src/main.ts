a9583e01007337354b772c0c7b0242c3d72f6960
Omri Shapira <OShapira@I-O-Tech.com>
2022-03-02 14:54:43 +0200

    update for prod

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  app.enableCors(); // protection
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
